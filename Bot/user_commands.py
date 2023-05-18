c2b1018e2500c53969cc55f93bc1ba7fb99051f1
ihazratummar <hazratummar9@gmail.com>
2023-05-18 18:18:11 +0530

    f

--- Bot/user_commands.py
+++ Bot/user_commands.py
@@ -1,24 +1,30 @@
-# region imports <- This is foldable 
+# region imports <- This is foldable
 import discord
 from client import bot
 from discord.ext import commands
 from discord.ext.commands import check
 import requests
 import os
-#endregion
+
+# endregion
 
 
 @bot.tree.command(name="ping", description="ping server")
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message(f"Pong! {bot.latency * 1000:.2f}ms")
-    
+
+
 @bot.tree.command(name="enroll", description="enrols a user to the server")
 async def enroll(interaction: discord.Interaction):
     link = "https://forms.gle/vBm5VpfPaExjrAm56"
-    await interaction.response.send_message(f"Enroll by clicking the link below:\n{link}")
-    
+    await interaction.response.send_message(
+        f"Enroll by clicking the link below:\n{link}"
+    )
+
+
 @bot.tree.command(name="instagram", description="instagram")
 async def instagram(interaction: discord.Interaction):
     link = "https://www.instagram.com/livenesports/"
-    await interaction.response.send_message(f"Check Out out official instagram account: {link}.")
-    
+    await interaction.response.send_message(
+        f"Check Out our official instagram account: {link}."
+    )
